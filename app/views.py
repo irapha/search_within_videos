4b923939c93f99699d4d9abf2cdece6de4fa826a
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2017-02-18 14:44:40 -0800

    refactor and return on api endpoint

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import redirect, render_template, render_template_string, Blueprint
 from flask import request, url_for, flash
 
+import app.lib.vision as v
 from app.init_app import app
 
 @app.route("/")
@@ -9,9 +10,10 @@ def home_page():
 
 @app.route("/add")
 def add():
-	return render_template('pages/add_video.html')
+    return render_template('pages/add_video.html')
 
 @app.route("/add_video", methods=['POST'])
 def add_video():
-	url = request.form["url"]
-	return "hello"
+    url = request.form["url"]
+    frames = v.get_labels_from_url(url)
+    return frames
